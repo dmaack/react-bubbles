8aa66da1a775cfd5b1877d626adcc922ec151415
dominique maack <dominiquemaack@gmail.com>
2019-10-18 10:25:12 -0600

    completed saveEdit and deleteColor functions - mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,24 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log('PUT data from SaveEdit', res)
+      setEditing(false)
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(res => {
+      console.log('delete data', res)
+      setEditing(false)
+    })
+    .catch(err => console.log(err))
   };
 
   return (
