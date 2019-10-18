ee5ba4b977413d99af44752abbd45bf47ed20fc0
dominique maack <dominiquemaack@gmail.com>
2019-10-18 11:39:18 -0600

    started playing with potion.js

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Pack } from "@potion/layout";
-import { Svg, Circle } from "@potion/element";
+import { Pack, Treemap } from "@potion/layout";
+import { Svg, Circle, Rect } from "@potion/element";
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
@@ -43,9 +43,46 @@ const Bubbles = ({ colors }) => {
                 return null;
               })
               .filter(v => v)
+              
           }
+          
         </Pack>
       </Svg>
+
+
+      {/* <Svg width={400} height={400}>
+        <Treemap
+          data={{
+            children: bubbleData
+          }}
+          sum={datum => datum.value}
+          size={[400, 400]}
+          nodeEnter={d => ({ ...d, r: 0 })}
+          animate
+        >
+        {nodes => 
+          nodes
+            .map(({ key, x0, y0, x1, y1 }, i) => {
+              if(i < colors.length) {
+                return (
+                <Rect
+                  key={key}
+                  x={x0}
+                  y={y0}
+                  width={x1 - x0}
+                  height={y1 - y0}
+                  fill="black"
+                  stroke="white"
+                />
+                );
+              }
+              return null
+            })
+            .filter(v => v)
+            }
+      </Treemap>
+  </Svg> */}
+
     </div>
   );
 };
